666cd155b4ce480e4b256ebae80ba2171daf3370
Trisha Pal <141509751+TrishaPal2004@users.noreply.github.com>
2026-08-17 12:50:25 +0530

    Changes

--- admin/server1.js
+++ admin/server1.js
@@ -10,10 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 // User Schema
